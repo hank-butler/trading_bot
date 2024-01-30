e57a5c3fb50a0a55d87e4c12667918029c4e2a1a
Hank <hank.butler@protonmail.com>
2024-01-29 21:01:41 -0700

    wrapping up imports

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -3,3 +3,6 @@ pragma solidity 0.8.19;
 
 // Adding import, reading in IVAUlT
 import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
