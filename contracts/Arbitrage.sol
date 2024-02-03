1bf515a800ae427a36e829cff9fba02379421354
Hank <hank.butler@protonmail.com>
2024-02-02 22:44:30 -0700

    notes for project

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -8,4 +8,11 @@ import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract Arbitrage is IFlashLoanRecipient {
     // bare bones of arbitrage contract
+
+    // So we're goign to build an arb bot.
+
+    // Going to need to take in different token prices from different dex's
+
+    // Provide liquidity
+    // and then do swaps
 }
--- test/Arbitrage.js
+++ test/Arbitrage.js
@@ -2,3 +2,10 @@ const { expect } = require('chai')
 
 const config = require('../config.json')
 
+describe('Arbitrage', () => {
+    let owner, arbitrage
+
+    // Get signers
+    // Deploy contract
+    // Need Sushi and UnisWap (or other Uni Swap fork)
+})
