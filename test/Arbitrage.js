b365ba2f661866e9efe85f5443e7b7df5738c48c
Hank <hank.butler@protonmail.com>
2024-02-04 20:38:28 -0700

    Update Arbitrage.js

--- test/Arbitrage.js
+++ test/Arbitrage.js
@@ -1,11 +1,22 @@
 const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
-const config = require('../config.json')
+const config = require('../config.json') // config file for doing things.
 
 describe('Arbitrage', () => {
     let owner, arbitrage
 
-    // Get signers
-    // Deploy contract
-    // Need Sushi and UnisWap (or other Uni Swap fork)
+    beforeEach(async () => {
+        [owner] = await ethers.getSigners()
+
+        arbitrage = await hre.ethers.deployContract(
+            "Arbitrage",
+            [
+                config.SUSHISWAP.V2_ROUTER_O2_ADDRESS,
+                config.UNISWAP.V2_ROUTER_O2_ADDRESS
+            ]
+        )
+
+        await arbitrage.waitForDeployment()
+    })
 })
