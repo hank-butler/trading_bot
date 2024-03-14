b1b897aa8474891d597060e39503bac90e1d9493
Hank <hank.butler@protonmail.com>
2024-03-13 21:13:35 -0600

    working on constructor

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -7,14 +7,27 @@ import "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract Arbitrage is IFlashLoanRecipient {
-    IVault private constant vault = IVault(); // what is this?
+    IVault private constant vault = IVault(); // what is this? Need address inside it.
 
     IUniswapV2Router02 public immutable sRouter;
     IUniswapV2Router02 public immutable uRouter;
     address public owner;
 
     constructor(address _sRouter, address _uRouter) {
+        sRouter = IUniswapV2Router(_sRouter); // Sushiswap
+        uRouter = IUniswapV2Router(_uRouter); // Uniswap
         // Add routers for uniswap, sushiswap
         owner = msg.sender; // person calling contract
+
     }
+
+    function executeTrade(
+        bool _startOnUniswap,
+        address _token0,
+        address _token1,
+        uint256 _flashAmount
+    ) external {
+        bytes memory data = abi.encode(_startOnUniswap, _token0, _token1);
+    }
+
 }
