d3167765d6110390b160290030243793774c9ae4
Hank <hank.butler@protonmail.com>
2024-03-24 22:02:36 -0600

    variables and module exports for initialization

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -11,3 +11,20 @@ if (config.PROJECT_SETTINGS.isLocal){
 } else {
     provider = new hre.ethers.WebSocketProvider(`wss://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`)
 }
+
+const uFactor = new hre.ethers.Contract(config.UNISWAP.FACTORY_ADDRESS, IUniswapV2Factory.abi, provider)
+const uRouter = new hre.ethers.Contract(config.UNISWAP.V2_ROUTER_02_ADDRESS, IUniswapV2Router02.abi, provider)
+const sFactory = new hre.ethers.Contract(config.SUSHISWAP.FACTORY_ADDRESS, IUniswapV2Factory.abi, provider)
+const sRouter = new hre.ethers.Contract(config.SUSHISWAP.V2_ROUTER_02_ADDRESS, IUniswapV2Router02.abi, provider)
+
+const IArbitrage = require('../artifcats/contracts/Arbitrage.sol/Arbitrage.json')
+const arbitrage = new hre.ethers.Contract(config.PROJECT_SETTINGS.ARBITRAGE_ADDRESS, IArbritrage.sol, provider)
+
+modole.exports = {
+    provider,
+    uFactor,
+    uRouter,
+    sFactory,
+    sRouter,
+    arbitrage
+}
