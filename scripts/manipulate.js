ac5b8431699e9d2d5aa41df1f7e2b19ac5784941
Hank <hank.butler@protonmail.com>
2024-03-26 15:18:23 -0600

    Update manipulate.js

--- scripts/manipulate.js
+++ scripts/manipulate.js
@@ -5,3 +5,5 @@ const hre = require("hre")
 const { getTokenAndConract, getPaircontract, calculatePrice } = require('../helpers/helpers')
 const { provider,uFactory, uRouter,  sFactory, sRouter } = require('../helpers/initialization')
 
+const V2_FACTORY_TO_USE = uFactory
+const V2_ROUTER_TO_USE = uRouter
