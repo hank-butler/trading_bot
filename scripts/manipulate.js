4fe2f0d8e4e19aaa839926723f7caade53c87d90
Hank <hank.butler@protonmail.com>
2024-03-27 17:43:13 -0600

    adding more constants, working on main()

--- scripts/manipulate.js
+++ scripts/manipulate.js
@@ -2,8 +2,29 @@ require("dotenv").config()
 
 const hre = require("hre")
 
-const { getTokenAndConract, getPaircontract, calculatePrice } = require('../helpers/helpers')
+const { getTokenAndCotract, getPaircontract, calculatePrice } = require('../helpers/helpers')
 const { provider,uFactory, uRouter,  sFactory, sRouter } = require('../helpers/initialization')
 
 const V2_FACTORY_TO_USE = uFactory
 const V2_ROUTER_TO_USE = uRouter
+
+const UNLOCKED_AMOUNT = '' // Fill this out later
+const AMOUNT = '40500000000000' // 40,500,000,000,000 SHIB -- Tokens will automatically be converted to wei
+
+async function main() {
+    const {
+        token0Contract,
+        token1Contract,
+        token0: ARB_AGAINST,
+        token1: ARB_FOR
+    } = await getTokenAndContract(process.env.ARB_AGAINST, process.env.ARB_FOR, provider)
+
+    const pair = await getPaircontract(V2_FACTORY_TO_USE, ARB_AGAINST.address, ARB_FOR.address, provider)
+
+    const priceBefore = await calculatePrice(pair)
+
+    await manipulatePrice([ARB_AGAINST, ARB_FOR], token0Contract)
+
+    co
+
+}
