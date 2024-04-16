7802fd98971a7ebd9be775c9d2015b3cb28f449c
Hank <hank.butler@protonmail.com>
2024-04-15 20:41:59 -0600

    start of main()

--- bot.js
+++ bot.js
@@ -16,3 +16,22 @@ const gasPrice = process.env.GAS_PRICE
 
 let uPair, sPair, amount
 let isExecuting = false
+
+const main = async () => {
+    const {token0Contract, token1Contract, token0, token1} = await getTokenAndContract(arbFor, arbAgainst, provider)
+    uPair = await getPairContract(uFactor, token0.address, token1.address, provider)
+    sPair = await getPairContract(sFactory, token0.address, token1.address, provider)
+
+    console.log(`uPair address: ${await uPair.getAddress()}`)
+    console.log(console.log('='.repeat(30)));
+    console.log(`sPair address: ${sPair.getAddress()}`)
+
+    uPair.on('Swap', async () => {
+        if (!isExecuting) {
+            isExecuting = true;
+
+            const priceDifference = await checkPrice('Uniswap', token0, token1)
+            const routerPath = await determineDirection(priceDifference)
+        }
+    }
+}
